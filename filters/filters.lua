96b88cda02b8f6945cfff7b617f5da51476281df
François Bastien <fmrbastien@gmail.com>
2025-04-04 18:28:33 +0200

    test ajout d’un mdbook github pages

--- filters/filters.lua
+++ filters/filters.lua
@@ -1,47 +1,74 @@
-function Span(el)
-	el = mk_span(el, "serpent")
-	return el
+-- Échappe les caractères LaTeX dangereux
+function escape_latex(str)
+	return str:gsub("\\", "\\textbackslash{}"):gsub("#", "\\#"):gsub("%%", "\\%%"):gsub("{", "\\{"):gsub("}", "\\}")
 end
 
-function Div(el)
-	local class_map = {
-		frontmatter = "\\frontmatter",
-		mainmatter = "\\mainmatter",
-		backmatter = "\\backmatter",
-	}
-
-	for class, cmd in pairs(class_map) do
-		if el.classes:includes(class) then
-			if FORMAT == "latex" then
-				local content = {}
-				for _, block in ipairs(el.content) do
-					table.insert(content, pandoc.write(pandoc.Pandoc({ block }), "plain"))
-				end
-				local latex_code = cmd .. "{" .. content .. "}"
-				return pandoc.RawBlock("latex", latex_code)
-			elseif FORMAT == "html" then
-				local html_code = '<div class="' .. class .. '">' .. pandoc.utils.stringify(el.content) .. "</div>"
-				return pandoc.RawBlock("html", html_code)
-			end
-		end
+-- Utilitaires : rendu markdown brut
+function inline_to_markdown(inlines)
+	local buffer = {}
+	for _, inline in ipairs(inlines) do
+		table.insert(buffer, pandoc.write(pandoc.Pandoc({ pandoc.Para({ inline }) }), "markdown"))
 	end
+	return table.concat(buffer):gsub("\n", " ")
+end
+
+function block_to_markdown(blocks)
+	local buffer = {}
+	for _, block in ipairs(blocks) do
+		table.insert(buffer, pandoc.write(pandoc.Pandoc({ block }), "markdown"))
+	end
+	return table.concat(buffer)
+end
+
+-- Utilitaires : rendu HTML
+function inline_to_html(inlines)
+	return pandoc.write(pandoc.Pandoc({ pandoc.Para(inlines) }), "html")
+end
+
+function block_to_html(blocks)
+	return pandoc.write(pandoc.Pandoc(blocks), "html")
+end
+
+-- Traitement des Spans avec classes
+function Span(el)
+	if #el.classes == 0 then
+		return el
+	end
+	local class_attr = ' class="' .. table.concat(el.classes, " ") .. '"'
+
+	-- Utiliser uniquement la première classe pour générer le nom de la macro
+	local class_name = el.classes[1]
+
+	if FORMAT == "latex" then
+		local latex_open_block = pandoc.RawInline("latex", "\\" .. class_name .. "{")
+		local latex_close_block = pandoc.RawInline("latex", "}")
+		table.insert(el.content, 1, latex_open_block)
+		table.insert(el.content, latex_close_block)
+	elseif FORMAT == "markdown" then
+		local content = inline_to_markdown(el.content)
+		return pandoc.RawInline("markdown", "<div" .. class_attr .. ">" .. content .. "</div>")
+	end
+
 	return el
 end
 
-function mk_span(el, class)
-	if el.classes:includes(class) then
-		local latex_open_block = pandoc.RawInline("latex", "\\" .. class .. "{")
-		local latex_close_block = pandoc.RawInline("latex", "}")
-		local html_open_block = pandoc.RawInline("html", '<span class="' .. class .. '">')
-		local html_close_block = pandoc.RawInline("html", "</span>")
+-- Traitement des Divs avec classes
+function Div(el)
+	if #el.classes > 0 then
+		-- Utiliser uniquement la première classe pour générer le nom de la macro
+		local class_name = el.classes[1]
+		local class_attr = ' class="' .. class_name .. '"'
 
 		if FORMAT == "latex" then
+			local latex_open_block = pandoc.RawBlock("latex", "\\" .. class_name .. "{")
+			local latex_close_block = pandoc.RawBlock("latex", "}")
 			table.insert(el.content, 1, latex_open_block)
 			table.insert(el.content, latex_close_block)
-		elseif FORMAT == "html" then
-			table.insert(el.content, 1, html_open_block)
-			table.insert(el.content, html_close_block)
+		elseif FORMAT == "markdown" then
+			local content = block_to_markdown(el.content)
+			return pandoc.RawBlock("markdown", "<div" .. class_attr .. ">" .. content .. "</div>")
 		end
 	end
-	return el -- Ne pas casser la structure
+
+	return el
 end
---
+++ test.md
@@ -0,0 +1,10 @@
+# Yolo
+::: {.frontmatter}
+:::
+
+::: {.intro}
+Paragraphe avec un [texte **important**]{.truc}.
+:::
+
+Yo Jazz, [tu *parles* Fourchelangue?]{.serpent} Parce que moi pas…
+
