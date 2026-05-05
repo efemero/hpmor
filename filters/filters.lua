6b87aacfeabff7e4791763dc069d48ceac4b10e7
François Bastien <fmrbastien@gmail.com>
2026-05-05 14:23:39 +0200

    fix: fourchelangue dans typst pour les pdf

--- filters/filters.lua
+++ filters/filters.lua
@@ -1,19 +1,19 @@
 function Underline(el)
 	if FORMAT == "markdown" then
-			local md_content = pandoc.write(pandoc.Pandoc({ pandoc.Para(el.content) }), "markdown")
-			md_content = md_content:gsub("^%s+", ""):gsub("%s+$", ""):gsub("\n", "")
-			local html = '<span class="underline">' .. md_content .. "</span>"
-			return pandoc.RawInline("markdown", html)
-	end	 
+		local md_content = pandoc.write(pandoc.Pandoc({ pandoc.Para(el.content) }), "markdown")
+		md_content = md_content:gsub("^%s+", ""):gsub("%s+$", ""):gsub("\n", "")
+		local html = '<span class="underline">' .. md_content .. "</span>"
+		return pandoc.RawInline("markdown", html)
+	end
 end
 
 function Span(el)
 	if #el.classes > 0 then
 		local class = el.classes[1]
-		if FORMAT == "latex" then
-			local latex_content = pandoc.write(pandoc.Pandoc({ pandoc.Para(el.content) }), "latex")
-			latex_content = latex_content:gsub("\n+$", "") -- supprime les fins de ligne
-			return pandoc.RawInline("latex", "\\" .. class .. "{" .. latex_content .. "}")
+		if FORMAT == "typst" then
+			local typst_content = pandoc.write(pandoc.Pandoc({ pandoc.Para(el.content) }), "typst")
+			-- typst_content = typst_content:gsub("\n+$", "") -- supprime les fins de ligne
+			return pandoc.RawInline("typst", "#" .. class .. "[" .. typst_content .. "]")
 		elseif FORMAT == "html" then
 			local md_content = pandoc.write(pandoc.Pandoc({ pandoc.Para(el.content) }), "markdown")
 			md_content = md_content:gsub("^%s+", ""):gsub("%s+$", ""):gsub("\n", "")
@@ -31,12 +31,12 @@ end
 function Div(el)
 	if #el.classes > 0 then
 		local class = el.classes[1]
-		if FORMAT == "latex" then
+		if FORMAT == "typst" then
 			local content = {}
 			for _, block in ipairs(el.content) do
-				local block_tex = pandoc.write(pandoc.Pandoc({ block }), "latex")
-				block_tex = block_tex:gsub("\n+$", "")
-				table.insert(content, block_tex)
+				local block_typst = pandoc.write(pandoc.Pandoc({ block }), "typst")
+				-- block_typst = block_tex:gsub("\n+$", "")
+				table.insert(content, block_typst)
 			end
 			local body = table.concat(content, "\n\n")
 			return pandoc.RawBlock("latex", "\\" .. class .. "{" .. body .. "}")
